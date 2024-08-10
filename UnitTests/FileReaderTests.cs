7587c4bea2679bc42c6951aa539c250ccebbd82f
deimantejagu <deimajagu@outlook.com>
2024-08-10 19:07:11 +0300

    some small fixes

--- UnitTests/FileReaderTests.cs
+++ UnitTests/FileReaderTests.cs
@@ -11,14 +11,15 @@ public class FileReaderTests
     {
         // Arrange
         const string fileName = "valid_transactions.txt"; 
-        var fileWriter = new StreamWriter(fileName); 
+        StreamWriter fileWriter = new StreamWriter(fileName); 
         fileWriter.WriteLine("2023-08-08 S LP 10.00");
         fileWriter.WriteLine("2023-08-09 M MR 5.00");
         fileWriter.Close();
-        var reader = new FileReader(fileName);
+
+        FileReader reader = new FileReader(fileName);
 
         // Act
-        var transactions = reader.GetTransactions();
+        List<string> transactions = reader.GetTransactions();
 
         // Assert
         Assert.Equal(2, transactions.Count);
@@ -32,8 +33,8 @@ public class FileReaderTests
     public void GetTransactions_NonExistentFile_ThrowsFileNotFoundException()
     {
         // Arrange
-        const string fileName = "non_existent_file.txt";
-        var reader = new FileReader(fileName);
+        const string fileName = "non_existing_file.txt";
+        FileReader reader = new FileReader(fileName);
 
         // Act & Assert
         Assert.Throws<FileNotFoundException>(() => reader.GetTransactions());
